b8f575c26ee427fd44afdc715146ea65156c19fa
vorber <vorber@gmail.com>
2025-12-04 00:06:27 +0200

    enable preview features, more day3 fiddling

--- day3.fs
+++ day3.fs
@@ -1,5 +1,5 @@
 namespace AOC
-
+#nowarn 57
 module Day3 =
     open Misc
     let input = [|
@@ -10,19 +10,18 @@ module Day3 =
     |]
 
     let private findMaxJoltage (digits:int array) =
-        let last = digits.Length - 1
         let findNext (pos, acc) skip =
             let nextPos = (+) (pos + 1)
             let nextAcc = int64 >> (+) (acc*10L)
-            let next = Array.indexed >> Array.maxBy snd >> tmap2 nextPos nextAcc
-            next digits[pos..(last-skip)]
+            let next = Seq.indexed >> Seq.maxBy snd >> tmap2 nextPos nextAcc
+            next digits[pos..^skip]
         flip Seq.init id >> Seq.rev >> Seq.fold findNext (0,0L) >> snd
 
-    let private overallMaxJoltage size = Array.sumBy (flip findMaxJoltage size)
+    let private overallMaxJoltage size = Seq.sumBy (flip findMaxJoltage size)
     let private part1 = overallMaxJoltage 2
     let private part2 = overallMaxJoltage 12
 
-    let parse = Array.map (Seq.map charToInt >> Seq.toArray)
+    let parse = Seq.map (Seq.map charToInt >> Seq.toArray)
 
     let solve: string seq -> string = 
-        Seq.toArray >> parse >> both part1 part2 >> output
+        parse >> both part1 part2 >> output
